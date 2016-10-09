e5b5168762d3ecb7070bc059440263247c4e49a8
vinaytejakoona <vinaytejakoona@gmail.com>
2016-10-09 15:00:05 +0530

    profile seekbars

--- app/src/main/java/com/apps/koona/managepower/profiles.java
+++ app/src/main/java/com/apps/koona/managepower/profiles.java
@@ -2,12 +2,84 @@ package com.apps.koona.managepower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 
 public class Profiles extends AppCompatActivity {
+
+    private SeekBar temperature1seekbar;
+    private TextView temperature1value;
+
+    private SeekBar humidity1seekbar;
+    private TextView humidity1value;
+
+    private SeekBar temperature2seekbar;
+    private TextView temperature2value;
+
+    private SeekBar humidity2seekbar;
+    private TextView humidity2value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
+        initializeVariables();
+
+
+
+
+        temperature1seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                //Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                temperature1value.setText(progress + "/" + seekBar.getMax());
+                //Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        humidity1seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                //Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+               // Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                humidity1value.setText(progress + "/" + seekBar.getMax());
+               // Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private void initializeVariables() {
+        temperature1seekbar = (SeekBar) findViewById(R.id.temperature1seekbar);
+        temperature1value = (TextView) findViewById(R.id.temperature1value);
+
+        humidity1seekbar = (SeekBar) findViewById(R.id.humidity1seekbar);
+        humidity1value = (TextView) findViewById(R.id.humidity1value);
+    }
+
 }

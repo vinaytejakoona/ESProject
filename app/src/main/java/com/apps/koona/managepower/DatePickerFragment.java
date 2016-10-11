5cc416aed202d8b03b804138775af843d9d40536
vinaytejakoona <vinaytejakoona@gmail.com>
2016-10-11 23:34:38 +0530

    date,time layout  added

--- app/src/main/java/com/apps/koona/managepower/DatePickerFragment.java
+++ app/src/main/java/com/apps/koona/managepower/DatePickerFragment.java
@@ -6,13 +6,12 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
-
+public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
@@ -22,11 +21,7 @@ public class DatePickerFragment extends DialogFragment
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
+        return new DatePickerDialog(getActivity(), (TimersActivity)getActivity(), year, month, day);
     }
 
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
-        Toast.makeText(getActivity(), "ON Date is set", Toast.LENGTH_SHORT).show();
-    }
 }
--- app/src/main/java/com/apps/koona/managepower/TimersActivity.java
+++ app/src/main/java/com/apps/koona/managepower/TimersActivity.java
@@ -1,19 +1,23 @@
 package com.apps.koona.managepower;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TimersActivity extends AppCompatActivity {
+public class TimersActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +35,72 @@ public class TimersActivity extends AppCompatActivity {
         ViewGroup insertPoint = (ViewGroup) findViewById(R.id.timersList);
 
         TextView textview;
+        EditText dateview;
+        EditText timeview;
         LinearLayout.LayoutParams layoutParams;
+
+        DatePicker datePickerView;
+
         for (int i = 0; i < timersList.size(); i++){
 
             // fill in any details dynamically here
+
             textview = new TextView(this);
+
             textview.setText(Integer.toString(timersList.get(i).getDeviceId()));
             layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
             // Add the text view to the parent layout
+            dateview = new EditText(this);
 
-            insertPoint.addView(textview,layoutParams);
+            dateview.setLayoutParams(layoutParams);
+            dateview.setHint("Date");
+            dateview.setFocusable(false);
+            dateview.setClickable(true);
+
+            dateview.setInputType(0x00000014);
+
+            dateview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DatePickerFragment datePickerFragment= new DatePickerFragment();
+                    datePickerFragment.show(getSupportFragmentManager(), "DatePicker");
+                }
+            });
+
+            timeview = new EditText(this);
+
+            timeview.setLayoutParams(layoutParams);
+            timeview.setHint("Time");
+            timeview.setFocusable(false);
+            timeview.setClickable(true);
+
+            timeview.setInputType(0x00000024);
+
+            timeview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TimePickerFragment timePickerFragment= new TimePickerFragment();
+                    timePickerFragment.show(getSupportFragmentManager(), "TimePicker");
+                }
+            });
+
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.setId(i);
+            linearLayout.addView(textview);
+            linearLayout.addView(dateview);
+            linearLayout.addView(timeview);
+            insertPoint.addView(linearLayout,layoutParams);
+
         }
     }
 
-
-
-
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        //do some stuff for example write on log and update TextField on activity
+        Log.d("DatePicker","Date = " + year);
+        //((EditText) findViewById(R.id.tf_date)).setText( day+"-"+month+"-"+year);
+    }
 
 }
